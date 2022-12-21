39cdd592b7530ed2d525208379640ebca2b3b68c
Atharva Shinde <atharvashinde179@gmail.com>
2022-12-22 01:16:09 +0530

    populater(): added logic to fetch and store required data by removing redundant stats

--- main.go
+++ main.go
@@ -32,21 +32,33 @@ func createFile() {
 	populater(usr, filePath, absPath)
 }
 
+// populates output.txt file with metrics from top utility
+// this function extracts pid,cpu metrics but it can track other metrics as well, to see options available try `top --help`
 func populater(usr, filePath, absPath string) {
-	getstats := exec.Command("top", "-l1", "-n10", "-stats", "pid,cpu")
-	output, err := getstats.CombinedOutput()
+	getrawstats := exec.Command("top", "-l1", "-n10", "-stats", "pid,cpu")
+	output, err := getrawstats.CombinedOutput()
 	if err != nil {
 		fmt.Print(err)
 	}
-	fmt.Print(string(output))
+	// fmt.Print(string(output))
 
-	//sed can also be used to delete the summary
+	rawpopulate := os.WriteFile(absPath, output, 0755)
+	if err != nil {
+		fmt.Print(err)
+	}
+	fmt.Print(rawpopulate)
 
-	populate := os.WriteFile(absPath, output, 0755)
+	skimmedstats := exec.Command("sed", "1,10d", absPath)
+	finaloutput, err := skimmedstats.CombinedOutput()
 	if err != nil {
 		fmt.Print(err)
 	}
 
-	fmt.Print(populate)
+	finalpopulate := os.WriteFile(absPath, finaloutput, 0755)
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	fmt.Print(finalpopulate)
 
 }
