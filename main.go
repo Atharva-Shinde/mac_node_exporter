e0b0de2199f7b8f408eea6ca9e4411f7b07713c8
Atharva Shinde <atharvashinde179@gmail.com>
2022-12-21 23:54:04 +0530

    chore: populate the text file with top utiity metrics:`pid and cpu`

--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
 )
 
 func main() {
 
 	createFile()
-	//TODO: populating output.txt with top metrics
 
 	//TODO: exporting metrics to prometheus
 }
@@ -22,11 +22,31 @@ func createFile() {
 	filePath := filepath.Join(usr, "Desktop")
 
 	// createfile, err := os.OpenFile(filepath.Join(filePath, "output.txt"), os.O_RDWR|os.O_CREATE, 0644)
-
-	createfile, err := os.Create(filepath.Join(filePath, "output.txt"))
+	absPath := filepath.Join(filePath, "output.txt")
+	createfile, err := os.Create(absPath)
 	if err != nil {
 		fmt.Print(err)
 	}
 	fmt.Print(createfile)
+
+	populater(usr, filePath, absPath)
+}
+
+func populater(usr, filePath, absPath string) {
+	getstats := exec.Command("top", "-l1", "-n10", "-stats", "pid,cpu")
+	output, err := getstats.CombinedOutput()
+	if err != nil {
+		fmt.Print(err)
+	}
+	fmt.Print(string(output))
+
+	//sed can also be used to delete the summary
+
+	populate := os.WriteFile(absPath, output, 0755)
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	fmt.Print(populate)
 
 }
