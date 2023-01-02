27a5def7c567e07fb737dd9aea50bdf321ef2406
Atharva Shinde <atharvashinde179@gmail.com>
2023-01-02 17:36:04 +0530

    Extractor() runs ./script.sh file and converts the result into float64

---
+++ extractor/extractor.go
@@ -0,0 +1,27 @@
+package extractor
+
+import (
+	"fmt"
+	"os/exec"
+	"strconv"
+	"strings"
+)
+
+// Extractor() runs ./script.sh file and converts the result into float64
+func Extractor() float64 {
+
+	cmd := exec.Command("/bin/bash", "./script.sh")
+	rawoutput, err := cmd.Output()
+	if err != nil {
+		fmt.Print(err)
+	}
+	finaloutput := string(rawoutput)
+	finaloutput = strings.TrimSpace(finaloutput)
+
+	result, err := strconv.ParseFloat(finaloutput, 64)
+	if err != nil {
+		panic(err)
+	}
+	// fmt.Print(result)
+	return result
+}
